9a394a51fb1f9f5438c2facf0488a840b0c07244
Wulnan Shadrach <wulnanshadrach268@gmail.com>
2025-01-23 00:39:28 +0000

    created QuotesContext, export the function QuotesProvider and pass in the value property to QuotesProvider render in the JSX

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,21 @@
 // Create your context here
+import React, {createContext} from 'react'
+
+export const QuotesContext = createContext()
+
+const value = {
+    car: 'Toyota Celica'
+}
+
+//export a function called QuotesProviver
+//it will take 'props' and return some jsx QuotesContext.Provider
+//and in there we will render props.children
+//props.children will become the part of React Tree we want to provide a value to.
+// we then pass in the 'value' props to QuotesContext.Provider 
+export function QuotesProviver(props) {
+  <QuotesContext.Provider value={value}>
+    {props.children}
+  </QuotesContext.Provider>
+}
+
+
